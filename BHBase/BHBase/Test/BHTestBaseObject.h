08c370fc65fe0a3c0d6d48251b9e38843231240c
raykle <sky8914@126.com>
2015-08-19 10:27:58 +0800

    no message

---
+++ BHBase/BHBase/Test/BHTestBaseObject.h
@@ -0,0 +1,13 @@
+//
+//  BHTestBaseObject.h
+//  BHBase
+//
+//  Created by guomin on 15/8/19.
+//  Copyright (c) 2015年 iBinaryOrg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BHTestBaseObject : NSObject
+
+@end
---
+++ BHBase/BHBase/Test/BHTestBaseObject.m
@@ -0,0 +1,13 @@
+//
+//  BHTestBaseObject.m
+//  BHBase
+//
+//  Created by guomin on 15/8/19.
+//  Copyright (c) 2015年 iBinaryOrg. All rights reserved.
+//
+
+#import "BHTestBaseObject.h"
+
+@implementation BHTestBaseObject
+
+@end
